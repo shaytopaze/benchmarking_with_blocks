f919e1ce5afaf1f2fcd72115564169a25e395457
Shay Alexander <shaytopazehair@gmail.com>
2018-02-26 19:17:18 +0000

    completed exercise

--- benchmark_with_block.rb
+++ benchmark_with_block.rb
@@ -1,11 +1,15 @@
 def benchmark
-  # Your benchmarking code goes here.
+  start_time = Time.now
+  end_time = Time.now
+
+  total_time = end_time - start_time
+  total_time
 end
- 
+
 # Be careful, pasting this into IRB will take a long time to print.
 # It's a loooong string. :)
 long_string = "apple"*100000000
- 
+
 running_time = benchmark { long_string.reverse }
- 
+
 puts "string.reverse took #{running_time} seconds to run"
